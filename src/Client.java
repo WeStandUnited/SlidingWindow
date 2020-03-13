e4e86b130ed9783d5c5edd54abfc0de82c1d9057
Cj <cchiass2@oswego.edu>
2020-03-13 13:56:47 -0400

    2nd commit

--- src/Client.java
+++ src/Client.java
@@ -1,2 +1,83 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.net.*;
+import java.nio.ByteBuffer;
+import java.util.Random;
+
+
 public class Client {
+
+
+    DatagramSocket sock =null;
+    DataInputStream in = null;
+    DataOutputStream out = null;
+
+
+    static int PORT;
+    static Inet4Address HOSTv4;
+    static Inet6Address HOSTv6;
+
+
+    public Client(int port,String host) throws UnknownHostException, SocketException {
+
+        PORT = port;
+        HOSTv4 = (Inet4Address) Inet4Address.getByName(host);
+        sock = new DatagramSocket();
+    }
+
+
+
+    public boolean Auth()  {
+        try {
+            Random r = new Random();
+            //Send Server SYN(Syn is just a random number)
+            int synnum = r.nextInt(500);
+            System.out.println("NumSent:"+synnum);
+            ByteBuffer buff = ByteBuffer.allocate(4);// 4 bytes is one int
+            buff.putInt(synnum);
+
+            byte[] b = buff.array();
+            DatagramPacket send = new DatagramPacket(b, b.length, HOSTv4, PORT);
+            buff.flip();
+            sock.send(send);
+            System.out.println("Packet Sent!");
+
+
+
+            return true;
+
+        }catch (IOException e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+
+
+    public void Send(File f){
+
+
+
+
+    }
+
+
+    public void Receive(){
+
+
+
+
+
+
+
+    }
+
+
+    public static void main(String[] args) throws UnknownHostException, SocketException {
+        Client c = new Client(2770,"localhost");
+
+        c.Auth();
+    }
 }
--- src/Server.java
+++ src/Server.java
@@ -1,2 +1,56 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.*;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+import java.util.Random;
+
 public class Server {
+
+
+
+    DataOutputStream out = null;
+    DataInputStream in = null;
+    DatagramSocket serverSocket = null;
+
+
+    static int PORT;
+    private int encrpytNum;
+
+
+
+    public Server(int port) throws IOException {
+
+        PORT = port;
+        serverSocket = new DatagramSocket(port);
+
+
+
+    }
+
+
+    public boolean Auth() throws IOException {
+        Random r = new Random();
+
+        int ACK = r.nextInt(500);//MAke ACK number
+
+        byte[] ACK_SYN_Staging = new byte[4];
+
+        DatagramPacket SYN = new DatagramPacket(ACK_SYN_Staging,ACK_SYN_Staging.length);
+
+        serverSocket.receive(SYN);
+        System.out.println("Packet Recieved!");
+
+
+        return true;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Server s =new Server(2770);
+
+            s.Auth();
+
+
+    }
 }
