c79cf27e6af8f9b8122c76f18c103c29de6238f8
Cj-Desktop <cchiass2@oswego.edu>
2020-03-17 17:39:33 -0400

    Auth Finished

--- src/Client.java
+++ src/Client.java
@@ -15,6 +15,14 @@ public class Client {
     DataOutputStream out = null;
 
 
+    static int WINDOW_SIZE;
+    static int STATE;
+    // STATE TABLE:
+    //0 = READ
+    //1 = WRITE
+    //2 = TERMINATE
+
+
     static int PORT;
     static Inet4Address HOSTv4;
     static Inet6Address HOSTv6;
@@ -32,16 +40,23 @@ public class Client {
     public int Auth() throws IOException {
         Random r = new Random();
 
-        int p = r.nextInt(600);//Public Modulo
-        int g= r.nextInt(600);//Public base
-        int a = r.nextInt(600);//Secret number
-        int A = (int) Math.pow(g,a) % p;
+        int mod_num = NumberGen.getPrime(600);//Public Modulo
+        int base= r.nextInt(600);//Public base
+        int secret_num = r.nextInt(600);//Secret number
+        int A = (int) Math.pow(base,secret_num) % mod_num;
 
         ByteBuffer buff = ByteBuffer.allocate(12);// Stuffing are byte buffer
-        buff.putInt(p);
-        System.out.println("Modulo:"+p);
-        buff.putInt(g);
-        System.out.println("Base:"+g);
+
+        ByteBuffer ACK_Buff = ByteBuffer.allocate(4);
+        DatagramPacket Ack = new DatagramPacket(ACK_Buff.array(),ACK_Buff.array().length);
+
+
+
+
+        buff.putInt(mod_num);
+        System.out.println("Modulo:"+mod_num);
+        buff.putInt(base);
+        System.out.println("Base:"+base);
         buff.putInt(A);
         System.out.println("Computed:"+A);
         buff.flip();
@@ -50,13 +65,24 @@ public class Client {
         DatagramPacket SYN  = new DatagramPacket(buff.array(),buff.array().length,HOSTv4,PORT);
         sock.send(SYN);
 
+        sock.receive(Ack);
+        ACK_Buff.put(Ack.getData());
+        ACK_Buff.flip();
+
+        int B = ACK_Buff.getInt();
+        System.out.println("Bob sent:"+B);
+
+        int s = (int)Math.pow(B,secret_num) % mod_num;
+        System.out.println("Key:"+s);
 
 
 
 
 
 
-        return 0;
+
+
+        return s;
     }
 
 
--- src/Server.java
+++ src/Server.java
@@ -32,31 +32,43 @@ public class Server {
 
     public int Auth() throws IOException {
         Random r = new Random();
-        int p;//Modulo num
-        int g;//base
-        int b = r.nextInt(600);
+        int mod_num;//Modulo num
+        int base;//base
+        int secret_Num = r.nextInt(600);//secret number
         int A;
         ByteBuffer buff = ByteBuffer.allocate(12);
-
+        ByteBuffer Ack_Buff = ByteBuffer.allocate(4);
 
         DatagramPacket SYN = new DatagramPacket(buff.array(),buff.array().length);
 
         serverSocket.receive(SYN);
         buff.put(SYN.getData());
         buff.flip();
-        p = buff.getInt();
-        System.out.println("Modulo:"+p);
-        g = buff.getInt();
-        System.out.println("Base:"+g);
+        mod_num = buff.getInt();
+        System.out.println("Modulo:"+mod_num);
+        base = buff.getInt();
+        System.out.println("Base:"+base);
         A = buff.getInt();
         System.out.println("Computed:"+A);
 
-        int B = (int) Math.pow(g,b) % p;
+
+        int B = (int) Math.pow(base,secret_Num) % mod_num;
+        Ack_Buff.putInt(B);
+        Ack_Buff.flip();
+        System.out.println("Sending:"+ B);
+
+        DatagramPacket ACK = new DatagramPacket(Ack_Buff.array(),Ack_Buff.array().length,SYN.getAddress(),SYN.getPort());
+        serverSocket.send(ACK);
+
+        int s = (int) Math.pow(A,secret_Num) % mod_num;
+
+
+        System.out.println("Key:"+s);
 
 
 
 
-        return 0;
+        return s;
     }
 
     public static void main(String[] args) throws IOException {
